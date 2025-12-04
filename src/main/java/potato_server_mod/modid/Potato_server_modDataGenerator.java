67a90f79b3e68ab35a985378ef58d8bf3ad1a10c
chen zhou <yuxinyang1207zhou@gmail.com>
2025-12-04 09:09:08 +0800

    Initial commit

---
+++ src/main/java/potato_server_mod/modid/Potato_server_modDataGenerator.java
@@ -0,0 +1,11 @@
+package potato_server_mod.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Potato_server_modDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
